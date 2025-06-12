fcd2075a0dd79394f215c17c248960c95fd8f9cf
Aamir | ecodeshub <ecodeshub@gmail.com>
2025-06-12 09:28:24 +0530

    navbar basic added

---
+++ src/lib/components/layout/navbar/navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Navlinks from "./navlinks.svelte";
+  import Navlogo from "./navlogo.svelte";
+</script>
+
+<header>
+  <div class="relative  w-11/12 mx-auto py-3">
+    <Navlogo />
+    <Navlinks />
+  </div>
+</header>
+
+<style>
+  header {
+    background-color: #060d01;
+  }
+</style>

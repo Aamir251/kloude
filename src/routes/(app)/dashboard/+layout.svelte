1d585c27b12353f76214424af986ad5e2b51b48b
Aamir | ecodeshub <ecodeshub@gmail.com>
2025-06-20 23:26:18 +0530

    manrope font added

--- src/routes/(app)/dashboard/+layout.svelte
+++ src/routes/(app)/dashboard/+layout.svelte
@@ -7,6 +7,12 @@
 <Navbar />
 
 
-<main class="container text-color-black">
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" >
+  <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
+</svelte:head>
+
+<main class="container text-color-black font-manrope">
   {@render children()}
 </main>

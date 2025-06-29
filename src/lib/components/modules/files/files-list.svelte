6834e37bd5d8caa95a37252f1fc6f991aff920fe
Aamir | ecodeshub <ecodeshub@gmail.com>
2025-06-29 23:28:58 +0530

    filelist item rendering

---
+++ src/lib/components/modules/files/files-list.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import NotFound from "@/components/ui/file/not-found.svelte";
+  import { getFileListItemIcon } from "@/utils/file-icon";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+  import type { Tables } from "../../../../database.types";
+
+  let { files, hasFiles = $bindable() } = $props()
+  
+  if (files.length) {
+    hasFiles = true
+  }
+  
+  let selectedFile : string = $state("")
+  let isDeleteFileDialogOpen : boolean = $state(false)
+
+  const handleDeleteFileClick = (id : string) => {
+    selectedFile = id
+    isDeleteFileDialogOpen = true
+  }
+  
+</script>
+
+{#if files?.length}
+  <div class="flex pt-10 flex-wrap gap-8">
+    {#each files as item}
+      {@render file(item)}
+    {/each}
+  </div>
+{:else}
+  <div class="not-found">
+    <NotFound />
+  </div>
+{/if}
+
+
+{#snippet file(item : Tables<"files">)}
+  <div class="py-3 pl-4 pr-1 rounded-md shadow border border-gray/20  w-72 flex justify-between">
+    <div  class="flex items-center gap-x-5 hover:text-gray transition ">
+      <div class="p-2.5 bg-gray/20 text-black rounded-md ">
+        <div class="w-6 h-6 ">
+          <img src="{getFileListItemIcon(item.type!)}" alt="{item.name}" />
+        </div>
+      </div>
+      <h3 class="text-sm font-bold">{item.name}</h3>
+    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger class="-translate-y-3 ">
+        <span class="bg-white pl-3 pr-1 py-2 rounded-sm font-semibold transition hover:opacity-80 cursor-pointer ">•••</span>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <DropdownMenu.Group class="font-medium">
+          <DropdownMenu.Item onclick={handleDeleteFileClick.bind(null, "id")}>Delete folder</DropdownMenu.Item>
+        </DropdownMenu.Group>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  </div>
+{/snippet}
---
+++ src/routes/folders/folders-page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import FilesList from '@/components/modules/files/files-list.svelte';
+  import FoldersList from '@/components/modules/folders/folders-list.svelte';
+
+  let { folders, files } = $props()
+0
+  let hasFiles : boolean = $state(false)
+
+</script>
+
+
+<div has-files="{hasFiles}">
+  {#await folders}
+    <h3>Loading Folders</h3> 
+  {:then resp}
+    {#if resp.error}
+      <h3>ERRORRRRRR {resp.error}</h3>
+    {:else}
+      <FoldersList folders={resp.data!} />
+    {/if}
+  {/await}
+
+  {#await files}
+    <h3>Loading Files</h3> 
+  {:then resp}
+    {#if resp.error}
+      <h3>ERRORRRRRR {resp.error}</h3>
+    {:else}
+      <FilesList files={resp.data!} bind:hasFiles  />
+    {/if}
+  {/await}
+</div>
+
+
+
